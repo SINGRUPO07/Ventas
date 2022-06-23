814d747885910bdd4f253c358ca70500d2840c6f
greciaAGI95 <grecia.garcia.i@alum.udep.edu.pe>
2022-06-23 00:31:38 -0500

    ultimo del díaaaaaaaaaaaaaaaaaaaaaaaaaa

--- registro_guardar.php
+++ registro_guardar.php
@@ -6,9 +6,17 @@
 
 		$usuarioError = null;
 		$contrasenaError = null;
+		$nombreError = null;
+		$apellidoError = null;
+		$correoError = null;
+		$telefonoError = null;
 
 		$usuario = $_POST['usuario'];
 		$contrasena = $_POST['contrasena'];
+		$nombre = $_POST['nombre'];
+		$apellido = $_POST['apellido'];
+		$correo = $_POST['correo'];
+		$telefono = $_POST['telefono'];
 
 		$valid = true;
 
@@ -20,13 +28,30 @@
 			$contrasenaError = 'Por favor escribe su contrasena';
 			$valid = false;
 		}
+		if (empty($nombre)) {
+			$nombreError = 'Por favor escribe su nombre';
+			$valid = false;
+		}
+		if (empty($apellido)) {
+			$apellidoError = 'Por favor escribe su apellido';
+			$valid = false;
+		}
+		if (empty($correo)) {
+			$correoError = 'Por favor escribe su correo';
+			$valid = false;
+		}
+		if (empty($telefono)) {
+			$telefonoError = 'Por favor escribe su telefono';
+			$valid = false;
+		}
 
 		if ($valid) {
 			$pdo = Database::connect();
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = 'INSERT INTO usuario (usuario, contrasena, fecha_registro, estado, tipo) values(?,?,NOW(),?,?)';
+			$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, correo, telefono, fecha_registro, estado, tipo) ";
+			$sql.= " values(?,?,?,?,?,?,NOW(),?,?)";
 			$q = $pdo->prepare($sql);
-			$q->execute(array($usuario, hash('sha3-512' , $contrasena), 0, 0));
+			$q->execute(array($usuario, hash('sha3-512' , $contrasena), $nombre, $apellido, $correo, $telefono, 1, 2));
 			Database::disconnect();
 			header("Location: login.php");
 		}
