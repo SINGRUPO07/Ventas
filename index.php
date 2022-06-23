fc86fa89c7064b087cdc07bdff9c38e1d067455f
greciaAGI95 <grecia.garcia.i@alum.udep.edu.pe>
2022-06-23 00:25:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-include_once("../modelos/usuarioDto.php");
+include_once("modelos/usuarioDto.php");
 ob_start();
 session_start();
 $oUsuario = isset($_SESSION['_usuario']) ? new Usuario($_SESSION['_usuario']) : null;
-if(!empty($oUsuario)) {
-} else {
-	header("Location: ../");
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,15 +13,16 @@ if(!empty($oUsuario)) {
 			<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-			<link href="../css/ventas.css" rel="stylesheet">
+			<link href="css/ventas.css" rel="stylesheet">
+			<script src="js/catalogo.js" type="text/javascript"></script>
 	</head>
 
 	<body class="d-flex flex-column h-100">
 		<header>
 			<div class="nav-inner">
 				<nav class="navbar navbar-expand-lg">
-					<a class="navbar-brand" href="../">
-						<img src="../img/logo.png" alt="Logo">
+					<a class="navbar-brand" href="./">
+						<img src="img/logo.png" alt="Logo">
 					</a>
 					<button class="navbar-toggler mobile-menu-btn active" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
 						<span class="toggler-icon"></span>
@@ -34,36 +31,40 @@ if(!empty($oUsuario)) {
 					</button>
 					<div class="navbar-collapse sub-menu-bar collapse show" id="navbarSupportedContent" style="">
 						<ul id="nav" class="navbar-nav ms-auto">
-							<p><?php echo $oUsuario->usuario; ?></p><br />
-							<li class="nav-item">
-								<a href="../" class="active" aria-label="Toggle navigation">Inicio</a>
-							</li>
-							<li class="nav-item">
-							<?php if(empty($oUsuario)) { ?>
-								<a href="login.php" aria-label="Toggle navigation">Iniciar sesión</a>
-							<?php } else { ?>
-								<?php if($oUsuario->tipo==1) { ?>
-									<a href="./" class="active" aria-label="Toggle navigation">Administración</a>
-								<?php } ?>
-								<a href="../logout.php" aria-label="Toggle navigation">Cerrar sesión</a>
+							<?php if(!empty($oUsuario)) { ?>
+								<p><?php echo $oUsuario->usuario; ?></p><br />
 							<?php } ?>
-							</li>
+
+								<?php if(empty($oUsuario)) { ?>
+									<li class="nav-item"><a href="login.php" aria-label="Toggle navigation">Iniciar sesión</a></li>
+								<?php } else { ?>
+									<?php if($oUsuario->tipo==1) { ?>
+										<li class="nav-item"><a href="./admin" class="active" aria-label="Toggle navigation">Administración</a></li>
+									<?php } else { ?>
+										<li class="nav-item">
+											<a href="#" id="cambiar-lugarentrega" class="active" aria-label="Toggle navigation" data-id="<?php echo isset($oUsuario->local_id) ? $oUsuario->local_id : "" ?>">
+												<?php echo isset($oUsuario->clocal) ? $oUsuario->clocal : "Sel. Local recojo" ?>
+											</a>
+										</li>
+										<li class="nav-item"><a href="./carrito" class="active" aria-label="Toggle navigation">Ver Carrito</a></li>
+									<?php } ?>
+									<li class="nav-item"><a href="logout.php" aria-label="Toggle navigation">Cerrar sesión</a></li>
+								<?php } ?>
 						</ul>
+					</div>
+					<div class="button">
+						<a href="registrar.php" class="btn btn-info">Registrarme</a>
 					</div>
 				</nav>
 			</div>
 		</header>
 
-		<main class="flex-shrink-0">
+		<main class="catalogo">
 			<div class="contenedor-menu">
 				<ul>
-					<li><a href="usuario">Gestión de Usuarios</a></li>
-					<li><a href="">Gestión de Productos</a></li>
-					<li><a href="">Gestión de Clientes</a></li>
-					<li><a href="">Gestión de Ordenes</a></li>
-					<li><a href="">Gestión de Ordenes</a></li>
-					<li><a href="">Gestión de Locales</a></li>
-					<li><a href="">Gestión de Medios de Pago</a></li>
+					<h5>Categorías</h5>
+					<ul id="lista-categorias">
+					</ul>
 				</ul>
 			</div>
 			<div class="contenedor-central">
@@ -77,5 +78,21 @@ if(!empty($oUsuario)) {
 				</span>
 		  </div>
 		</footer>
+
+		<div id="modal-detalle" class="modal" tabindex="-1">
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title"></h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+					</div>
+					<div class="modal-footer">
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</body>
 </html>
