171fd53224a1f61672c0db1e29dee4a38fb34036
NeiverVR <106372504+NeiverVR@users.noreply.github.com>
2022-06-24 01:04:14 -0500

    modelo actualizado

---
+++ modelos/categoriaDto.php
@@ -0,0 +1,22 @@
+<?php
+class Categoria {
+	public $id;
+	public $nombre;
+
+	public function __construct($properties=[]){
+        if (!empty($properties)) {
+            array_walk($properties, function ($val, $key) {
+                $this->fromArray($key, $val);
+            });
+        }
+    }
+
+    public function fromArray($property, $value){
+        return (property_exists($this, $property)) ? $this->$property = $value : null;
+    }
+	public function toArray()
+  {
+      return get_object_vars($this);
+  }
+}
+?>
