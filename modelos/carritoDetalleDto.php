110a5d591eb27d46c99dfd9a3de90b24f2ef2711
Nathali Paz Echevarria <nathali.paz@alum.udep.edu.pe>
2022-06-24 01:15:54 -0500

    edité carrito

---
+++ modelos/carritoDetalleDto.php
@@ -0,0 +1,24 @@
+<?php
+class CarritoDetalle {
+	public $id;
+	public $producto_id;
+	public $cantidad;
+	public $estado;
+
+	public function __construct($properties=[]){
+        if (!empty($properties)) {
+            array_walk($properties, function ($val, $key) {
+                $this->fromArray($key, $val);
+            });
+        }
+    }
+
+    public function fromArray($property, $value){
+        return (property_exists($this, $property)) ? $this->$property = $value : null;
+    }
+	public function toArray()
+  {
+      return get_object_vars($this);
+  }
+}
+?>
